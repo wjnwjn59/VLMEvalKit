49cd1973a88b7bf5017a0936c3a342e717995a70
Koii2k3 <devkoi2k3@gmail.com>
2025-11-14 02:25:21 +0700

    speed up base64

--- LMUData/convert2base64.py
+++ LMUData/convert2base64.py
@@ -3,27 +3,36 @@ import base64
 from PIL import Image
 import io
 from tqdm import tqdm
+from multiprocessing import Pool, cpu_count
 
-df = pd.read_csv('~/LMUData/our_raw_dataset.tsv', sep='\t')
+df = pd.read_csv('/home/khoina/LMUData/NarrativeInfoVQA_Test/val_full_data.tsv', sep='\t')
 
-def encode_image_to_base64(image_path):
-    with Image.open(image_path) as img:
-        buffered = io.BytesIO()
-        img.save(buffered, format="PNG")
-        return base64.b64encode(buffered.getvalue()).decode()
+def encode_row(row):
+    img_path, question, answer, idx = row
 
-data = []
-for i, row in tqdm(df.iterrows(), total=len(df)):
-    img_path = row['image_path']
-    question = row['question']
-    answer = row['answer']
-    
-    data.append({
-        'index': i,
+    try:
+        with Image.open(img_path) as img:
+            buffered = io.BytesIO()
+            img.save(buffered, format="PNG")
+            img_base64 = base64.b64encode(buffered.getvalue()).decode()
+    except Exception as e:
+        print(f"Error image {img_path}: {e}")
+        img_base64 = ""
+
+    return {
+        'index': idx,
         'question': question,
         'answer': answer,
-        'image': encode_image_to_base64(img_path),
-    })
+        'image': img_base64
+    }
 
-df = pd.DataFrame(data)
-df.to_csv('~/LMUData/our_dataset/our_dataset.tsv', sep='\t', index=False)
+rows = [(df.iloc[i]["image_path"], 
+         df.iloc[i]["question"],
+         df.iloc[i]["answer"],
+         i) for i in range(len(df))]
+
+with Pool(cpu_count()) as p:
+    results = list(tqdm(p.imap(encode_row, rows), total=len(rows)))
+
+df_out = pd.DataFrame(results)
+df_out.to_csv('/home/khoina/LMUData/our_dataset.tsv', sep='\t', index=False)
